f7d977b91c7807554220757915b49e931df7c21a
Jon Rice <revyolution1120@gmail.com>
2020-04-07 16:13:54 -0700

    Working on initial tutorial notes and examples.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+//These are our import statements. They hold the information that we need within the statements so we can use them later. They also allow the file to use anything that is imported this way. 
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+// The function below is used to kickstart our Application. What it does when called is return the html that is shown in the return statement. 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// We can instead of using a function to return our desired html we can set up a class through Component. This allows use to use the state of the component. State is similar to props, but it is private and fully controlled by the component.
+class App extends Component {
+// Constructor functions are typically used for two things: Initializing local state by assigning an object to this.state or Binding event handler methods to an instance. Constructor is the only place where you should assign this.state directly. In all other methods, you need to use this.setState() instead. Here we can see we are using it for local state and with our button we are using an event handler to change the state. 
+  constructor(){
+    super();
+    this.state = {
+      welcomeString:'Welcome to the React tutorial'
+    }
+  }
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.welcomeString} 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick ={() => this.setState({welcomeString:'Let us see what we can do'})}>Click to change text</button>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// This sets up our virtual DOM. The library of ReactDOM calls and renders what is in the App function that we set up in the App.js file. It is rendered within the root element on the index.html. This is done by the statement within the function below by getting the elementById. 
 ReactDOM.render(
   <React.StrictMode>
     <App />
