6f63d9cb47093328475ecc06694f1757f2014a91
Jon Rice <revyolution1120@gmail.com>
2020-04-08 20:13:24 -0700

    Working on the first part of the rolodex.

--- src/App.js
+++ src/App.js
@@ -26,27 +26,63 @@ import './App.css';
 // }
 
 // We can instead of using a function to return our desired html we can set up a class through Component. This allows use to use the state of the component. State is similar to props, but it is private and fully controlled by the component.
+// class App extends Component {
+// // Constructor functions are typically used for two things: Initializing local state by assigning an object to this.state or Binding event handler methods to an instance. Constructor is the only place where you should assign this.state directly. In all other methods, you need to use this.setState() instead. Here we can see we are using it for local state and with our button we are using an event handler to change the state. 
+//   constructor(){
+//     super();
+//     this.state = {
+//       welcomeString:'Welcome to the React tutorial'
+//     }
+//   }
+//   render() {
+//     return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           {this.state.welcomeString} 
+//         </p>
+//         <button onClick ={() => this.setState({welcomeString:'Let us see what we can do'})}>Click to change text</button>
+//       </header>
+//     </div>
+//     );
+//   }
+// }
+
+//Here is where we are going to start really working on setting up our rolodex. 
 class App extends Component {
-// Constructor functions are typically used for two things: Initializing local state by assigning an object to this.state or Binding event handler methods to an instance. Constructor is the only place where you should assign this.state directly. In all other methods, you need to use this.setState() instead. Here we can see we are using it for local state and with our button we are using an event handler to change the state. 
-  constructor(){
-    super();
-    this.state = {
-      welcomeString:'Welcome to the React tutorial'
-    }
+
+constructor() {
+  super();
+//We set the state of the monsters array with objects that contain the name and an id. It is needed to have an id or a property that can be used as a key within the html so that if the element changes React knows where to go to make that change. 
+  this.state = {
+    monsters: [
+      {
+        name: "First name",
+        id:'ID1'
+      },
+      {
+        name:'Second name',
+        id: 'ID2'
+      },
+      {
+        name:'Thrid name',
+        id: 'ID3'
+      }
+    ]
   }
+}
 
   render() {
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {this.state.welcomeString} 
-        </p>
-        <button onClick ={() => this.setState({welcomeString:'Let us see what we can do'})}>Click to change text</button>
-      </header>
-    </div>
-    );
+      <div className='App'>
+      {
+        // The .map() function used here will go through the monsters array and pick out the id and name setting those to the key and the text of the h1 element respectively. 
+        this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
+      }
+      </div>
+  
+    )
   }
 }
 
