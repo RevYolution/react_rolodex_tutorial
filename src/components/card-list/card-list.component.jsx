12d31944c610682cf47670df5019e63a76268fce
Jon Rice <revyolution1120@gmail.com>
2020-04-12 17:52:29 -0700

    small changes to make app appear as grid.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './card-list.styles.css';
+
 // The props that we are utilizing in this method is any information that we are passing to this component where it is being used. We can see that in this example when we view the console we see all of the properties that are being stated in the App.js where the CardList is being utilized. 
 export const CardList = props => {
     console.log(props);
 
-    return <div>CardList component</div>;
+    return <div className='card-list'>{props.children}</div>;
 };
